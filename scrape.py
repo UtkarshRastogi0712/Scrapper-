a5644e255f9f623af2f2df153e64403b258489dc
UtkarshRastogi0712 <utkarshrastogi0712@gmail.com>
2022-08-27 00:44:01 +0530

    Implemented basic functionality

--- scrape.py
+++ scrape.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-url='https://www.grainmart.in/news/covid-19-coronavirus-india-state-and-district-wise-tally/'
+url='https://www.flipkart.com/gaming/gaming-consoles/pr?sid=4rr,x1m&otracker=categorytree'
 page=requests.get(url)
 print(page)
+soup = BeautifulSoup(page.content,'html.parser')
+data=soup.find('a',class_='s1Q9rs')
+print(data.text)
