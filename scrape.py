e420bb7f2e45c8fab7c49506baa0c481380ba47c
UtkarshRastogi0712 <utkarshrastogi0712@gmail.com>
2022-09-01 12:04:22 +0530

    Extracted names

--- scrape.py
+++ scrape.py
@@ -5,5 +5,5 @@ url='https://www.flipkart.com/gaming/gaming-consoles/pr?sid=4rr,x1m&otracker=cat
 page=requests.get(url)
 print(page)
 soup = BeautifulSoup(page.content,'html.parser')
-data=soup.find('a',class_='s1Q9rs')
-print(data.text)
+for name in soup.find_all('a',class_='s1Q9rs'):
+    print(name.text)
